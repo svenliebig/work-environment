79a03c8a3d945b76d43c07ccf91d4dcf84d26c7a
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2022-12-07 15:12:34 +0100

    feat(cli): add diffent user input methods

--- pkg/utils/cli/user_input.go
+++ pkg/utils/cli/user_input.go
@@ -4,8 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func QuestionFree(q string) string {
+	fmt.Print(q)
+	reader := bufio.NewReader(os.Stdin)
+	i, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("an error occured while reading input. Please try again", err)
+		return ""
+	}
+
+	i = strings.TrimSuffix(i, "\n")
+	return i
+}
 
 func Question(q string, allowedAnswers []string) string {
 	fmt.Print(q)
@@ -30,6 +45,46 @@ func Question(q string, allowedAnswers []string) string {
 	}
 
 	return i
+}
+
+func QuestionMulti(q string, options []string) []string {
+	fmt.Println()
+	fmt.Println(q)
+	fmt.Println()
+
+	for i, o := range options {
+		fmt.Printf("  (%d): %s\n", i+1, o)
+	}
+
+	fmt.Print("\nSelect the options (komma separated): ")
+	reader := bufio.NewReader(os.Stdin)
+	i, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("an error occured while reading input. Please try again", err)
+		return []string{}
+	}
+
+	selected := make([]string, 0, len(options))
+	answers := strings.Split(strings.TrimSuffix(i, "\n"), ",")
+
+	for _, a := range answers {
+		v, err := strconv.Atoi(a)
+
+		if err != nil {
+			fmt.Println("an error occured while reading input. Please try again", err)
+			return []string{}
+		}
+
+		if v <= len(options) && v > 0 {
+			selected = append(selected, options[v-1])
+		} else {
+			fmt.Println("please only choose answers that are available for choice")
+			return QuestionMulti(q, options)
+		}
+	}
+
+	return selected
 }
 
 func QuestionScanner(q string, allowedAnswers []string) string {
